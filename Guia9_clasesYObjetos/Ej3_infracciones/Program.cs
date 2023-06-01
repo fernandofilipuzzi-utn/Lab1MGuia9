989c7a0be189fca610be82002ee2b7ebe7d1c63f
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-06-01 09:30:07 -0300

    agregando ejercicio 3

---
+++ Guia9_clasesYObjetos/Ej3_infracciones/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej3_infracciones
+{
+    class Program
+    {
+
+        static void PantallaIngresoActa(SistemaActas sistema)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Ingrese dni y nombre");
+            int dni = Convert.ToInt32(Console.ReadLine());
+            string nombre = Console.ReadLine();
+            sistema.IniciarActa(dni, nombre);
+
+            Console.WriteLine("Ingrese codigo infraccion(1 a 5 , 0 terminar)");
+            int codigo = Convert.ToInt32(Console.ReadLine());
+            while (codigo > 0)
+            {
+                double monto = sistema.RegistrarInfraccion(codigo);
+                Console.WriteLine($"Monto de la infracción({codigo}): $ {monto:f2}");
+
+                Console.WriteLine("Ingrese codigo infraccion(1 a 5 , 0 terminar)");
+                codigo = Convert.ToInt32(Console.ReadLine());
+            }
+            sistema.FinalizarActa(true);
+            Console.WriteLine("Total a pagar: {0:f2}", sistema.totalAPagar);
+
+            Console.WriteLine("Presione una tecla para volver al menu");
+            Console.ReadKey();
+        }
+
+        static void PantallaImprimirDia(SistemaActas sistema)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Recaudación: {0:f2}", sistema.Recaudacion);
+
+            Console.WriteLine("Presione una tecla para volver al menu");
+            Console.ReadKey();
+        }
+
+        static ConsoleKeyInfo PantallaMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1- Registrar acta");
+            Console.WriteLine("2- Consulta recaudación");
+            Console.WriteLine("otro- salida");
+            return Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            SistemaActas sis = new SistemaActas();
+
+            #region pantalla de configuracion
+            Console.Clear();
+            Console.WriteLine("Iniciando sistema");
+            Console.WriteLine("Ingrese el valor Base (litros nafta/$)");
+            double montoBase = Convert.ToDouble(Console.ReadLine());
+            sis.IniciarSistema(montoBase);
+            #endregion
+
+            ConsoleKeyInfo key;
+            do
+            {
+                key=PantallaMenu();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+                        {
+                            #region pantalla de carga de acta
+                            PantallaIngresoActa(sis);
+                            #endregion
+                        }
+                        break;
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+                        {
+                            #region pantalla de carga de acta
+                            PantallaImprimirDia(sis);
+                            #endregion
+                        }
+                        break;
+                }
+            } while (key.Key!= ConsoleKey.D0 && key.Key != ConsoleKey.NumPad0);
+
+
+        }
+    }
+}
---
+++ Guia9_clasesYObjetos/Ej3_infracciones/SistemaActas.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej3_infracciones
+{
+    class SistemaActas
+    {
+        #region nomenclador de infracciones
+        int CODIGO_1_INFRACCION = 1;
+        string DESC_1_INFRACCION = "Sin luces bajas, ley 25….";
+        int UD_1_INFRACCION = 25;
+        //
+        int CODIGO_2_INFRACCION = 2;
+        string DESC_2_INFRACCION = "alta de Matafuego, ley 2…";
+        int UD_2_INFRACCION = 30;
+        #endregion
+
+        #region atributos generales de sistema
+        double montoBase;
+
+        public double Recaudacion { get; private set; }
+
+        /*
+        double recaudacion;
+        public double Recaudacion
+        {
+            get {
+                    ...
+                return recaudacion;
+            }
+            set
+            {
+                recaudacion = value;
+            }
+        }
+        */
+
+        /*
+        double recaudacion;
+        public double GetRecaudacion()
+        {
+            return recaudacion;
+        }
+        public void SetRecaudacion(double Value)
+        {
+            recaudacion = Value;
+        }
+        */
+        #endregion
+
+        #region atributos por cada acta
+        int dni;
+        string nombre;
+        public double totalAPagar;
+        #endregion
+
+        #region atributos por cada infraccion
+        int codigoInfraccion;
+        string descInfraccion;
+        double montoInfraccion;
+        #endregion
+
+        #region método del sistema
+        public void IniciarSistema(double montoBase)
+        {
+            this.montoBase = montoBase;
+            this.Recaudacion = 0;
+        }
+        #endregion
+
+        #region métodos por acta
+        public void IniciarActa(int dni, string nombre)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+            totalAPagar = 0;
+        }
+
+        public double RegistrarInfraccion(int codigo)
+        {
+            switch (codigo)
+            {
+                case 1:
+                    {
+                        montoInfraccion = UD_1_INFRACCION * montoBase;
+                    }
+                    break;
+                case 2:
+                    {
+                        montoInfraccion = UD_2_INFRACCION * montoBase;
+                    }
+                    break;
+            }
+            totalAPagar += montoInfraccion;
+            return montoInfraccion;
+        }
+
+        public void FinalizarActa(bool pagaEnElLugar)
+        {
+            Recaudacion += totalAPagar;
+        }
+        #endregion
+
+    }
+}
